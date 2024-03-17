1e9292db4a1e3e28ea49f173b2b32e8ad7727754
RealTheBeastBoss <98669913+RealTheBeastBoss@users.noreply.github.com>
2024-03-17 17:11:43 +0000

    Working on the Starting UI Mock-up Part One

---
+++ Scenes and Scripts/solo_create_character.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes and Scripts/solo_screen.tscn")
+
+func _on_done_pressed():
+	pass # Replace with function body.
--- Scenes and Scripts/title_screen.gd
+++ Scenes and Scripts/title_screen.gd
@@ -3,16 +3,16 @@ extends Control
 # The "Button" nodes will be converted into "TextureButton" nodes once we have the textures
 
 func _on_quit_pressed():
-	get_tree().quit()
+	get_tree().quit() # This is the easiest bit of code I've every typed in my life
 
-func _on_solo_pressed():
+func _on_solo_pressed(): # The Code below allows one to change the scene currently being processed
 	get_tree().change_scene_to_file("res://Scenes and Scripts/solo_screen.tscn")
 
 func _on_band_pressed():
 	pass
 
 func _on_settings_pressed():
-	pass
+	get_tree().change_scene_to_file("res://Scenes and Scripts/settings_screen.tscn")
 
 func _on_help_pressed():
 	pass
