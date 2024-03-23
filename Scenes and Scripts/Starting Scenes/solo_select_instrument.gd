e2af844c96f67f3d6b251192696cea0e4a7969d5
RealTheBeastBoss <98669913+RealTheBeastBoss@users.noreply.github.com>
2024-03-23 21:59:03 +0000

    Vinyl Selection Active

--- Scenes and Scripts/Starting Scenes/solo_select_instrument.gd
+++ Scenes and Scripts/Starting Scenes/solo_select_instrument.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
+var selection = "Top"
+@onready var instrument = $Instrument
+
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes and Scripts/Starting Scenes/solo_create_character.tscn")
+
+func _on_vinyl_selector_selection_changed(new_selection):
+	selection = new_selection
+	instrument.text = selection
--- Scenes and Scripts/Starting Scenes/vinyl_selector.gd
+++ Scenes and Scripts/Starting Scenes/vinyl_selector.gd
@@ -4,7 +4,16 @@ var did_click = false
 var is_dragging = false
 var click_position : Vector2 = Vector2.ZERO
 var move_delta : Vector2 = Vector2.ZERO
+var selection = "Top"
+var last_rotation = 0
+
+signal selection_changed(new_selection)
+
 @onready var vinyl = $Area2D/Vinyl
+@onready var top = $Area2D/Vinyl/Top
+@onready var right = $Area2D/Vinyl/Right
+@onready var left = $Area2D/Vinyl/Left
+@onready var bottom = $Area2D/Vinyl/Bottom
 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
@@ -16,10 +25,30 @@ func _input(event: InputEvent):
 		move_delta = click_position - event.position
 	if Input.is_action_just_released("left_click"):
 		did_click = false
+		last_rotation = vinyl.rotation_degrees
 
 func _physics_process(delta):
 	if is_dragging and did_click:
-		vinyl.rotation_degrees = -move_delta.x
+		vinyl.rotation_degrees = last_rotation - move_delta.x
+		# Gets the Rotation of the Vinyl within 360:
+		var degrees = vinyl.rotation_degrees
+		while degrees > 360:
+			degrees -= 360
+		while degrees < 0:
+			degrees += 360
+		# Gets the current selection:
+		var new_selection = null
+		if degrees > 315 or degrees < 45:
+			new_selection = "Top"
+		elif degrees > 225:
+			new_selection = "Right"
+		elif degrees > 135:
+			new_selection = "Bottom"
+		else:
+			new_selection = "Left"
+		if new_selection != selection:
+			selection = new_selection
+			selection_changed.emit(selection)
 
 func _on_vinyl_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("left_click"):
